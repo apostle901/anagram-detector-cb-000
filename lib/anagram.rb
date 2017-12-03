27321c14dd775cd86af9601873a385ebdfb87b59
Jawann <jawann@gmail.com>
2017-12-03 22:36:47 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+def Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.select {|x| x.split.sort == @word.split.sort}
+  end
+end
